18b2d4658908f9e9ac78d53bff76d1ac2b29aa1c
Cuthbert <brian.j.cuthbert@accenture.com>
2020-12-22 12:23:24 +0000

    Attempted Scorekey - comment ViewEncapsulation

--- src/app/score-key/score-key.component.ts
+++ src/app/score-key/score-key.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-score-key',
   templateUrl: './score-key.component.html',
-  styleUrls: ['./score-key.component.css']
+  styleUrls: ['./score-key.component.css'],
+  // encapsulation: ViewEncapsulation.None
   
 })
 export class ScoreKeyComponent implements OnInit {
