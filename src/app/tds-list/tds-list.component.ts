6ed43b0f9fdbdb5028aa0fde1949910faae2d627
BrianUplift <110971071+BrianUplift@users.noreply.github.com>
2024-06-06 10:56:24 +0100

    Updated Ennistymon to Ennistimon

--- src/app/tds-list/tds-list.component.ts
+++ src/app/tds-list/tds-list.component.ts
@@ -105,7 +105,7 @@ export class TdsListComponent implements OnInit {
     'Edenderry',
     'Ennis',
     'Enniscorthy',
-    'Ennistymon',
+    'Ennistimon',
     'Fermoy',
     'Firhouse - Bohernabreena',
     'Galway City Central',
